8bec0dec40084819d92276ab0aabb50c929b5350
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2019-01-04 16:16:24 +0000

    Modify rake task output

--- app/providers/notify_provider.rb
+++ app/providers/notify_provider.rb
@@ -30,13 +30,13 @@ class NotifyProvider
     :technical_failure
   end
 
-  def get_notify_email_status(id)
-    puts "Notification details for email with id #{id}"
-    notification = client.get_notification(id)
-
-    puts "Status: #{notification.status}"
-    puts "Response: #{notification.response}"
-  end
+  # def get_notify_email_status(id)
+  #   puts "Notification details for email with id #{id}"
+  #   notification = client.get_notification(id)
+  #
+  #   puts "Status: #{notification.status}"
+  #   puts "Response: #{notification.response}"
+  # end
 
   def get_notify_multiple_email_status(reference)
     #reference is the DeliveryAttempt.id
@@ -46,12 +46,35 @@ class NotifyProvider
       template_type: 'email',
       reference: reference
     )
+    # puts "Response =  #{response}"
+    if response.is_a?(Notifications::Client::NotificationsCollection)
+      count = response.collection.count
 
-    puts "Response =  #{response}"
+      puts "Found #{count} notification(s)"
 
-    count = response.collection.count
+      response.collection.each do |notification|
+        puts <<~TEXT
+          -------------------------------------------
+          Notification ID: #{notification.id}
+          Status: #{notification.status}
+          created_at: #{notification.created_at}
+          sent_at: #{notification.sent_at}
+          completed_at: #{notification.completed_at}
+        TEXT
 
-    puts "Found #{count} results"
+        # puts "==================================="
+        # puts "Notification id: #{notification.id}"
+        # puts "Status: #{notification.status}"
+        # puts "created_at: #{notification.created_at}"
+        # puts "sent_at: #{notification.sent_at}"
+        # puts "completed_at: #{notification.completed_at}"
+      end
+
+    elsif response.is_a?(Notifications::Client::RequestError)
+      puts "Query for #{reference} returns error code #{error.code}, #{error.message}"
+    else
+      puts "Reference not found"
+    end
   end
 
 private
